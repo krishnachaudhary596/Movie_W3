32fcf1d54e7bad1e477efd46dd9e2f455e221f2c
Krishna Chaudhary <krishnachaudhary596@gmail.com>
2023-05-09 22:33:32 +0530

    Completed basic module

--- Movie_Query.sql
+++ Movie_Query.sql
@@ -29,4 +29,22 @@
 	LEFT JOIN rating r ON m.mov_id = r.mov_id
 	GROUP BY m.mov_title, m.mov_dt_rel, r.num_o_ratings
 
-	
+--7. Retrieve the name of all the actors and movie title who acted in a particular movie (movie_id = 912):
+	SELECT a.act_fname, a.act_lname, m.mov_title
+	FROM actor a
+	INNER JOIN movie_cast mc ON a.act_id = mc.act_id
+	INNER JOIN movie m ON m.mov_id = mc.mov_id
+	WHERE mc.mov_id = 912
+
+--8. Retrieve the name of all the directors who directed a particular movie (movie_id = 908):
+	SELECT d.dir_fname, d.dir_lname, m.mov_title
+	FROM director d
+	INNER JOIN movie_direction md ON d.dir_id = md.dir_id
+	INNER JOIN movie m ON md.mov_id = m.mov_id
+	WHERE md.mov_id = 908
+
+--9. Retrieve the name of all the reviewers, review stars, number of ratings for a particular movie (movie_id = 906):
+	SELECT r.rev_name, ra.rev_stars, ra.num_o_ratings
+	FROM reviewer r
+	INNER JOIN rating ra ON r.rev_id = ra.rev_id
+	WHERE ra.mov_id = 903
